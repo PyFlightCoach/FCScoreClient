8bbb0b9f3f1519953906c18a4f81dd4160674cb5
Thomas David <thomasdavid0@gmail.com>
2023-09-20 05:17:28 +0100

    version stuff

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Flight Coach Score Client
 
-docker build -t fcs_client --build-arg SOURCE_COMMIT=$SOURCE_COMMIT .
+docker build -t fcs_client --build-arg SOURCE_COMMIT=$(git rev-parse HEAD) .
 
 docker run --rm -p 5173:5173 --name=fcs_client fcs_client
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
   import {  Heading, P } from 'flowbite-svelte'
   import {server_version} from '$lib/api_calls';
+  import {onMount} from 'svelte';
 
-  let version: string = 'no connection';
-  server_version().then(v=>{version=v});
-  let client_version: string = '0.0.1';
-  
+  let version: string;
+
+  onMount(()=>{
+    server_version().then(v=>{version=v});
+  });
+      
+  import {PUBLIC_VERSION} from '$env/static/public';
+
 </script>
 
   <div>
@@ -13,9 +18,9 @@
     <div>WIP example of automatic judging process based on Flight Coach JSON files</div>    
 
     <br/>
-    <div>Client:{client_version}</div>
+    <div>Client:{PUBLIC_VERSION}</div>
     <div>Server:{version}</div>
-    
+
 
 
   </div>
