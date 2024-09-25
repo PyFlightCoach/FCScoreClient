6f283e58282bfbb6ee043bf34b9e6144d34347ea
Thomas David <thomasdavid0@gmail.com>
2024-09-25 09:15:40 +0100

    add busy info to loading page

--- src/routes/upload/+page.svelte
+++ src/routes/upload/+page.svelte
@@ -6,7 +6,7 @@
 	import Splitter from './Splitter.svelte';
 	import { Origin } from '$lib/api_objects/fcjson';
 	import BoxPlot from './BoxPlot.svelte';
-	import { ButtonGroup, RadioButton, Button, P, List, Li , Heading, Tooltip} from 'flowbite-svelte';
+	import { ButtonGroup, RadioButton, Button, P, List, Li, Heading, Tooltip } from 'flowbite-svelte';
 	import { serverFunc } from '$lib/api_calls';
 	import { MA } from '$lib/api_objects/ma';
 	import {
@@ -16,30 +16,36 @@
 		fcj,
 		isCompFlight,
 		manoeuvres,
-		states, activeHelp, binData
+		states,
+		activeHelp,
+		binData
 	} from '$lib/stores';
-  import { createAnalyses, clearAnalysis} from '$lib/analysis';
+	import { createAnalyses, clearAnalysis } from '$lib/analysis';
 	import { goto } from '$app/navigation';
 	import { base } from '$app/paths';
 	import { ManSplit } from '$lib/splitting';
 	import { get } from 'svelte/store';
-  import Info from './Info.svelte';
-	
-  
+	import Info from './Info.svelte';
+
 	clearAnalysis();
 
 	let mans: ManSplit[];
 	let activeTab = 'Info';
 	let boxKind = 'F3A';
+	let busy = false;
 
-  $: $activeHelp = activeTab;
+	$: $activeHelp = activeTab;
 
 	const handleNewBin = () => {
 		if ($binData.hasOwnProperty('ORGN[1]') && !$fcj) {
 			if (
 				!$origin ||
 				GPS.sub(
-					new GPS($binData['ORGN[1]'].Lat[0], $binData['ORGN[1]'].Lng[0], $binData['ORGN[1]'].Alt[0]),
+					new GPS(
+						$binData['ORGN[1]'].Lat[0],
+						$binData['ORGN[1]'].Lng[0],
+						$binData['ORGN[1]'].Alt[0]
+					),
 					new GPS($origin.lat, $origin.lng, $origin.alt)
 				).length() > 300
 			) {
@@ -52,7 +58,6 @@
 			}
 		}
 		activeTab = 'Box';
-    
 	};
 
 	const handleNewFCJ = () => {
@@ -61,9 +66,15 @@
 	};
 
 	async function createState() {
+		busy = true;
 		if ($bin && $origin) {
 			states.set(
-				States.parse(await serverFunc('create_state', { data: $binData, site: $origin.noMove() }, 'POST'))
+				States.parse(
+					await serverFunc(
+						'create_state', {data: $binData, site: $origin.noMove() },
+						'POST'
+					)
+				)
 			);
 			activeTab = 'Manoeuvres';
 		} else if ($fcj) {
@@ -74,6 +85,7 @@
 			);
 			activeTab = 'Manoeuvres';
 		}
+		busy = false;
 	}
 
 	function createAnalysis() {
@@ -105,9 +117,9 @@
 		goto(base + '/analysis');
 	}
 
-  $: if($states==undefined && activeTab=='Manoeuvres'){
-    activeTab = 'Info';
-  }
+	$: if ($states == undefined && activeTab == 'Manoeuvres') {
+		activeTab = 'Info';
+	}
 </script>
 
 <div class="parent">
@@ -136,7 +148,7 @@
 			{/if}
 		</ButtonGroup>
 	</div>
-  <div class="bg">
+	<div class="bg">
 		<ButtonGroup>
 			<RadioButton bind:group={activeTab} value={'Info'}>Info</RadioButton>
 			<RadioButton bind:group={activeTab} value={'Box'} disabled={!$binData}>Box</RadioButton>
@@ -145,29 +157,41 @@
 			>
 		</ButtonGroup>
 	</div>
-  {#if $fcj && $states && $fcj.data.length != $states.getFCJLength()}
-  <ButtonGroup>
-    <P size='sm' color="text-red-700 dark:text-red-500">Warning: FCJ.length={$fcj.data.length}, st.length={$states.getFCJLength()}</P>
-    <Button on:click={()=>{$fcj=undefined}}>dissconnect FCJ</Button>
-  </ButtonGroup>
-  <Tooltip>
-    Either the FCJ was created from a different BIN file (probably not good), 
-    or the flight coach plotter used a different set of sources (probably fine). 
-    Double check splitting and consider dissconnecting FCJ file from this analysis.
-  </Tooltip>
-  {/if}
+	{#if $fcj && $states && $fcj.data.length != $states.getFCJLength()}
+		<ButtonGroup>
+			<P size="sm" color="text-red-700 dark:text-red-500"
+				>Warning: FCJ.length={$fcj.data.length}, st.length={$states.getFCJLength()}</P
+			>
+			<Button
+				on:click={() => {
+					$fcj = undefined;
+				}}>dissconnect FCJ</Button
+			>
+		</ButtonGroup>
+		<Tooltip>
+			Either the FCJ was created from a different BIN file (probably not good), or the flight coach
+			plotter used a different set of sources (probably fine). Double check splitting and consider
+			dissconnecting FCJ file from this analysis.
+		</Tooltip>
+	{/if}
 	<div class="display">
-		{#if activeTab == 'Info'}
-      <div class="info">
-        <Info/>
-        <Heading tag="h4">Loaded Files:</Heading>
-        <List>
-          <Li>Bin File: {$bin?.name}</Li>
-          <Li>FC json: {$fcj?.name}</Li>
-          
-        </List>
-      </div>
-      
+		{#if busy}
+			<div class="loading">
+				<Heading tag="h4">Loading...</Heading>
+				<P
+					>This can take a minute or two, for better performance try running the analysis server
+					locally</P
+				>
+			</div>
+		{:else if activeTab == 'Info'}
+			<div class="info">
+				<Info />
+				<Heading tag="h4">Loaded Files:</Heading>
+				<List>
+					<Li>Bin File: {$bin?.name}</Li>
+					<Li>FC json: {$fcj?.name}</Li>
+				</List>
+			</div>
 		{:else if activeTab == 'Box'}
 			<BoxPlot bind:binData={$binData} bind:origin={$origin} bind:kind={boxKind} />
 		{:else if activeTab == 'Manoeuvres'}
@@ -177,25 +201,36 @@
 </div>
 
 <style>
-  .bg {
-    justify-self: center;
-  }
+	.bg {
+		justify-self: center;
+	}
 	.display {
 		grid-column: 1/4;
 		height: 100%;
-    width: 100%;
-    margin-left: auto; margin-right: auto;
+		width: 100%;
+		margin-left: auto;
+		margin-right: auto;
+		align-content: stretch;
 	}
 	.parent {
 		height: 100%;
-    width: 100%;
+		width: 100%;
 		justify-self: center;
 		display: grid;
 		grid-template-columns: 2fr 1fr 1fr;
 		grid-template-rows: min-content 1fr;
 	}
-  .info {
-    width: 70%; justify-self: center; margin-left: auto; margin-right: auto;
-  }
-
+	.info {
+		width: 70%;
+		justify-self: center;
+		margin-left: auto;
+		margin-right: auto;
+	}
+	.loading {
+		width: 70%;
+		justify-self: center;
+		margin-left: auto;
+		margin-right: auto;
+		align-self: center;
+	}
 </style>
